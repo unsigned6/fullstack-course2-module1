7e2bd8f057618b5c7b21dc36d6cb7aecefdbef90
unsigned6 <yuriivlasiuk@gmail.com>
2015-11-11 13:57:43 +0200

    Get by id methods

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -7,16 +7,10 @@ class Assignment
   # Insert rows in DB
   #
   def create_user(params)
-      # accept a hash of user properties (`:username` and `:password_digest`) as an input parameter. Note these are 100% same as model class.
-      # use the User Model class to create a new user in the DB
-      # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
       user = User.create(params)
   end
 
   def create_todolist(params)
-      # accept a hash of todolist properties (`:name` and `:due_date`) as an input parameter. Note these are not 100% the same as Model class.
-      # use the TodoList Model class to create a new user in the DB
-      # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
       todolist = TodoList.create(list_name: params[:name], list_due_date: params[:due_date])
   end
 
@@ -24,16 +18,10 @@ class Assignment
   # Retrieve paginated results from DB
   #
   def find_allusers(offset, limit)
-      # accept offset and limit input parameters
-      # use the User Model class to find all Users, ordered by `updated_at` ascending, with specified row offset and row limit
-      # return a collection of User instances that represent the specified rows
       User.offset(offset).limit(limit).all.order(:updated_at)
   end
 
   def find_alllists(offset, limit)
-      # accept offset and limit input parameters
-      # use the TodoList Model class to find all TodoLists, ordered by `list_due_date` descending, with specified row offset and row limit
-      # return a collection of TodoList instances that represent the specified rows
       TodoList.offset(offset).limit(limit).all.order(list_due_date: :desc)
   end
 
@@ -41,18 +29,10 @@ class Assignment
   # Query DB with exact match
   #
   def find_user_byname(username)
-      # accept a username input parameter
-      # use the User Model class to find all Users with the supplied username. 
-      # NOTE:  Username is not unique in the Users table, thus you can have many users with the same username.
-      # return a collection of User instances that match the provided username
       User.where(username: username)
   end 
 
   def find_todolist_byname(name)
-      # accept a name input parameter
-      # use the TodoList Model class to find all TodoLists with the supplied list_name. 
-      # NOTE: List name is not required to be unique, thus you can have many lists with the same list name.
-      # return a collection of TodoList instances that match the provided name
       TodoList.where(list_name: name)
   end     
 
@@ -60,15 +40,14 @@ class Assignment
   # Get rows from DB by PK
   #
   def get_user_byid(id)
-      # accept an id input parameter
-      # use the User Model class to get the User associated with the `id` primary key
-      # return the User instance that matches the provided id
+      User.find(id)
   end
 
   def get_todolist_byid(id)
       # accept an id input parameter
       # use the TodoList Model class to get the TodoList associated with the `id` primary key
       # return the TodoList instance that matches the provided id
+      TodoList.find(id)
   end
 
   #
@@ -102,13 +81,14 @@ class Assignment
   end
 end
 a = Assignment.new
-#h = {username: "j", password_digest: "12345"}
-#d = Time.now
+
 #h_list = {list_name: "john list", list_due_date: "2015-11-12"}
-h_list2 = {name: "johnlist2", due_date: "2015-11-13"}
-#a.create_user(h)
+#h_list2 = {name: "johnlist2", due_date: "2015-11-13"}
+#a.create_user({username: "jane", password_digest: "qwerty"})
 #a.create_todolist(h_list)
-a.create_todolist(h_list2)
+#a.create_todolist(h_list2)
 #puts a.find_allusers(5,5)
 #puts a.find_alllists(3,2)
-puts a.find_todolist_byname("johnlist2")
+#puts a.find_todolist_byname("johnlist2")
+#puts a.get_user_byid(94)
+puts a.get_todolist_byid(67)
